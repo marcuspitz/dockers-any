6b86174d2f41f42a1500b99f3a320fd4643e1601
marcuspitz <marcusviniciuspitz@gmail.com>
2022-10-28 14:34:58 -0300

    Remove sudo from init sh

--- local-spanner/initspanner.sh
+++ local-spanner/initspanner.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo gcloud config configurations create emulator
-sudo gcloud config set auth/disable_credentials true
-sudo gcloud config set project coronavirus-272213
-sudo gcloud config set api_endpoint_overrides/spanner http://spanner:9020/
+gcloud config configurations create emulator
+gcloud config set auth/disable_credentials true
+gcloud config set project coronavirus-272213
+gcloud config set api_endpoint_overrides/spanner http://spanner:9020/
 
-sudo gcloud spanner instances create spanner-emulator --config=emulator-config --description="Emulated" --nodes=1
-sudo gcloud spanner databases create time_slots --instance=spanner-emulator --ddl-file=time-slots.ddl
+gcloud spanner instances create spanner-emulator --config=emulator-config --description="Emulated" --nodes=1
+gcloud spanner databases create time_slots --instance=spanner-emulator --ddl-file=time-slots.ddl
